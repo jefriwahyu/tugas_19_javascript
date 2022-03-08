8fae88e22418c2afe6dc211ef37e075307fba9e4
Jefriwahyu <memberjefry1@gmail.com>
2022-03-08 10:45:11 +0700

    Tugas 19 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,14 @@
+function bintang(){
+  var bintangKecil = "*****";
+  console.log(bintangKecil);
+  console.log(bintangKecil.repeat(5));
+  console.log(bintangKecil.repeat(9));
+  console.log(bintangKecil.repeat(13));
+  console.log(bintangKecil.repeat(19));
+  console.log(bintangKecil.repeat(13));
+  console.log(bintangKecil.repeat(9));
+  console.log(bintangKecil.repeat(5));
+  console.log(bintangKecil.repeat(1));
+}
+
+bintang();
